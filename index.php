7fb4ccde86b6c22edf8f7334fab5cd6985a7b686
Mirell Metsnik <mirell.metsnik@tptlive.ee>
2026-09-02 13:26:56 +0300

    g

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 echo "Hello, World!";
 ?>
+// hello
+// world
+
+/**
+*
+*long comment
+**/
+
+$name = "John Doe"; // Variable to store the nameˇ
+$age = 30; // Variable to store the age
+echo $age;
+$age = $age * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10; // Multiply age by 2, 3, 4, 5, 6, 7, 8, 9, and 10 
+echo $age // output the updated age
