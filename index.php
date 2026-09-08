707b6803ca50cc1604d8124359a6fbb953a509e0
Mirell Metsnik <mirell.metsnik@tptlive.ee>
2026-09-08 11:14:16 +0300

    if else case 123456

--- index.php
+++ index.php
@@ -1,8 +1,39 @@
 <?php
-$test = 'hello';
-$test = 'hello' . 'world';
-$test = '!';
-$name = 'mirell';
-$age = 19;
-$test = $name . ' is ' . $age . ' years old.';
-var_dump($test);
+$num = 10;
+if($num > 5){
+    var_dump('bigger');
+}
+
+if($num > 5){
+ var_dump('bigger');   
+} else {
+    var_dump('smaller');
+}
+
+if($num > 5){
+ var_dump('bigger');   
+} else if ($num <5) {
+    var_dump('smaller');
+}
+
+$i = 1;
+
+switch($i) {
+    case 1:
+      var_dump('yx'); 
+      break;
+    case 2:
+        var_dump('kaks'); 
+        break;
+    case 3:
+    case 4:
+        var_dump('kolm neli');
+        break;
+    case 5:
+        var_dump('viis');
+    case 6:
+        var_dump('kuus');
+        break;
+    default:
+        var_dump('dunno');
+}
