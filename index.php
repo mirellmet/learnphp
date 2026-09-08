6762f68b54f0f45d6fcd8f34b6a9c1b6c5306704
Mirell Metsnik <mirell.metsnik@tptlive.ee>
2026-09-08 11:32:40 +0300

    while

--- index.php
+++ index.php
@@ -1,39 +1,41 @@
 <?php
-$num = 10;
-if($num > 5){
-    var_dump('bigger');
+
+for($i=0; $i<10; $i++){
+    var_dump($i);
+}
+for($i=9; $i>=0; $i--){
+    var_dump($i);
 }
 
-if($num > 5){
- var_dump('bigger');   
-} else {
-    var_dump('smaller');
+for($i=2; $i<=1_000_000; $i*=2){
+    var_dump($i);
 }
 
-if($num > 5){
- var_dump('bigger');   
-} else if ($num <5) {
-    var_dump('smaller');
+$time = time();
+$n = 0;
+while($time+1 > time()) {
+    $n++;
+}
+var_dump($n);
+
+while(false) {
+    var_dump('While');
 }
 
-$i = 1;
+do {
+    var_dump('Do ');
+} while(false);
 
-switch($i) {
-    case 1:
-      var_dump('yx'); 
-      break;
-    case 2:
-        var_dump('kaks'); 
-        break;
-    case 3:
-    case 4:
-        var_dump('kolm neli');
-        break;
-    case 5:
-        var_dump('viis');
-    case 6:
-        var_dump('kuus');
-        break;
-    default:
-        var_dump('dunno');
+// while(true) {
+
+// }
+
+$fruits = ['apple', 'banana', 'orange'];
+
+foreach($fruits as $fruit) {
+    var_dump($fruit);
+}
+
+foreach($fruits as $key => $fruit) {
+    var_dump($key, $fruit);
 }
