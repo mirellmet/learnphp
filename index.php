a8e1567b7a75bcf51d71ae87e4c680ce551f6adc
Mirell Metsnik <mirell.metsnik@tptlive.ee>
2026-09-09 13:54:07 +0300

    c

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
 <?php
-$test = 10;
-$test = 3.1;
-$test = "Hello World";
-$test = "õpilane";
-$test = true;
-$test = false;
-$test = null;
-$test = new stdClass();
-$test->name = "Mirell";
-var_dump($test);
-$num1 = "10";
-$num2 = "100";
-var_dump($num === $num2);
+$num1 = 1;
+$num2 = $num1;
+$num1 = 2;
+var_dump($num1, $num2);
 
-$test = [1,2, "three", true];
-var_dump($test);
+$box1 = new Box();
+$box1->width = 2;
+$box2 = $box1;
+$box2->width = 2;
+var_dump($box1, $box2);
