d44023ad78905ef45232c63e8b0d6e38de34d4f7
Mirell Metsnik <mirell.metsnik@tptlive.ee>
2026-09-11 13:12:04 +0300

    private

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
-$num1 = 1;
-$num2 = $num1;
-$num1 = 2;
-var_dump($num1, $num2);
 
-$box1 = new Box();
-$box1->width = 2;
-$box2 = $box1;
-$box2->width = 2;
-var_dump($box1, $box2);
+class Box {
+    private $w;
+    private $h;
+    private $l;
+
+    public function __construct($w, $h, $l){
+        $this ->w = $w;
+        $this ->h = $h;
+        $this ->l = $l;
+        var_dump('Box was created');
+    }
+    public function volume(){
+        return $this->w * $this->h * $this->l;
+    }
+}
